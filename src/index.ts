cbc150e778327dfd8fbabad58181fc530d1eefc1
David Spiller <david.spiller@external.toyota-europe.com>
2024-06-07 10:08:27 +0200

    Various changes

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import {updateWiki} from "./wiki";
+
+// GitHub token and repository provided by the GitHub Actions runner
+const token = process.env['GITHUB_TOKEN'];
+const repository = process.env['GITHUB_REPOSITORY']; // Format: owner/repo
+const wikiRepositoryUrl = `https://github-actions:${token}@github.com/${repository}.wiki.git`;
+
+const pagePath = 'Category/SubCategory/NewPage2.md';
+
+const pageContent = "## Sub Title\nThis is new content";
+
+updateWiki(wikiRepositoryUrl, pagePath, pageContent);
---
+++ src/wiki.ts
@@ -0,0 +1,33 @@
+import {execSync} from 'child_process';
+
+export function updateWiki(wikiUrl: string, pagePath: string, pageContent: string) {
+
+    try {
+        // Set git config
+        execSync('git config --global user.name "github-actions[bot]"');
+        execSync('git config --global user.email "github-actions[bot]@users.noreply.github.com"');
+
+        // Clone the wiki repository
+        execSync(`git clone ${wikiUrl} .wiki`, {stdio: 'inherit'});
+
+        // Navigate to the wiki repository directory
+        process.chdir('.wiki');
+
+        // Ensure the directories exist
+        execSync(`mkdir -p $(dirname ${pagePath})`);
+
+        // Create or update a wiki page
+        execSync(`echo "${pageContent}" >> ${pagePath}`);
+
+        // Commit and push the changes
+        execSync(`git add ${pagePath}`);
+        execSync('git commit -m "Update wiki via GitHub Actions"');
+        execSync('git push origin HEAD:master', {stdio: 'inherit'});
+
+        console.log('Wiki updated successfully.');
+    } catch (error) {
+        console.error('Error updating wiki:', error);
+        process.exit(1);
+    }
+
+}
