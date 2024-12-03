f3a8c6ba985dfeea129173dfad7cdb32b6c8e449
David Spiller <dspi@users.noreply.github.com>
2024-12-03 12:55:15 +0100

    Update renameCollections.ts

--- src/scripts/renameCollections.ts
+++ src/scripts/renameCollections.ts
@@ -1,5 +1,25 @@
-export const renameCollections = () => {
+import {closeDB, connectToDB, getDB} from '../utils/mongodb';
+
+export const renameCollections = async (collections: string[], prefix: string): Promise<void> => {
     console.log('This is "renameCollections"');
+    
+    const db = await connectToDB(); // Ensure the DB is connected
+
+    try {
+        for (const oldName of collections) {
+            const newName = `${prefix}${oldName}`;
+            try {
+                console.log(`Renaming: ${oldName} -> ${newName}`);
+                await db.collection(oldName).rename(newName);
+            } catch (error) {
+                console.error(`Failed to rename collection ${oldName}`);
+            }
+        }
+    } catch (error) {
+        console.error('Error during renaming collections:', error);
+    } finally {
+        await closeDB(); // Close the connection here
+    }
 }
 
-renameCollections();
+renameCollections(['doesNotExist', `launches`], 'hardcoded_');
