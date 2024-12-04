fda6c5dd610b5167b9b8e17cf6a7086d785575ed
David Spiller <dspi@users.noreply.github.com>
2024-12-04 18:13:54 +0100

    Update saySomething.ts

--- src/scripts/saySomething.ts
+++ src/scripts/saySomething.ts
@@ -1,6 +1,11 @@
-export const run = (something: string): string =>  {
+// export const run = (something: string): string =>  {
+//     console.log(something);
+//     return `SUCCESS...wrote "${something}"`;
+// }
+
+export const run = (something: string) =>  {
     console.log(something);
-    return `SUCCESS...wrote "${something}"`;
+    //return `SUCCESS...wrote "${something}"`;
 }
 
 run('This is the saySomethiing script!');
