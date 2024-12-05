392cbf916cee350f1918ccd3e2cf1e1fb1917196
David Spiller <dspi@users.noreply.github.com>
2024-12-05 22:29:06 +0100

    Update example-script.ts

--- src/scripts/example-script.ts
+++ src/scripts/example-script.ts
@@ -4,7 +4,7 @@ import { ScriptRunResult, Status } from "../definitions";
  * All scripts must execute a run function, which returns a ScriptRunResult type.
  */
 export const run = (): ScriptRunResult => {
-    return {
+    const runResult = {
         overallStatus: Status.FAILURE,
         details: [
             { status: Status.SUCCESS, resource: "MongoDB collection 5", info: "Line 1 blah was successful" },
@@ -12,6 +12,11 @@ export const run = (): ScriptRunResult => {
             { status: Status.SKIPPED, resource: "MongoDB collection 31", info: "Line 3 blah didn't run!" }
         ]
     };
+
+    //Is this needed to get the into the workflow?
+    //console.log(JSON.stringify(runResult));
+
+    return runResult;
 };
 
 run();
