4414b73f8a04696c87680a5f065efb4f52f11368
David Spiller <dspi@users.noreply.github.com>
2024-12-06 10:30:22 +0100

    Update validation.ts

--- src/helpers/validation.ts
+++ src/helpers/validation.ts
@@ -1,5 +1,13 @@
 import { ScriptRunResult } from "../definitions";
 
+/**
+ * This function is designed to pass a ScriptRunResult back to the initiating GitHub Action.
+ *
+ * Additionally, it is useful to have TypeScript's static type-checking to ensure the parameter is of
+ * type 'ScriptRunResult'.  This simplifies the validation (for the Wiki entry) in the GitHub Action,
+ * to ensure acceptable output before the script is run.
+ * @param runResult
+ */
 export const validate = (runResult:ScriptRunResult) => {
     console.log(JSON.stringify(runResult));
 }
